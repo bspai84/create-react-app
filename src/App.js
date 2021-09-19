b46dbe2812663f1c4f8033ebeccc1164cfab8cdf
bspai84 <46858824+bspai84@users.noreply.github.com>
2021-09-19 23:34:06 +0530

    Update App.js

--- src/App.js
+++ src/App.js
@@ -10,7 +10,7 @@ class App extends Component {
           <img src={logo} className="App-logo" alt="logo" />
           <p>
             Hello from Render!
-              This is for testing PR preview....
+              This is for testing branch preview....
           </p>
           <a
             className="App-link"
